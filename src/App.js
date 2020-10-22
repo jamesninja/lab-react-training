6bf38b95eb962dd02dff18ccff614c716a45bfb1
toufiq23 <drrameraaron023@gmail.com>
2020-10-22 21:01:36 +0200

    iteration 3 on going

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import IdCard from './components/idcard/IdCard';
+import Greetings from './components/greetings/Greetings';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+	render(){
+		
+		return (
+			<div className="App">
+				<div>
+					<h3>IdCard</h3>
+					<IdCard
+						lastName='Doe'
+						firstName='John'
+						gender='male'
+						height={178}
+						// birth={new Date("1992-07-14")}
+						birth="1992-07-14"
+						picture="https://randomuser.me/api/portraits/men/44.jpg"
+					/>
+
+					<IdCard
+						lastName='Delores '
+						firstName='Obrien'
+						gender='female'
+						height={172}
+						// birth={new Date("1988-05-11")}
+						birth="1988-05-11"
+						picture="https://randomuser.me/api/portraits/women/44.jpg"
+					/>
+				</div>
+				<div>
+					<h3>Greetings</h3>
+					<div>
+						<Greetings lang='de'>John</Greetings>
+						<Greetings lang='en'>Mike</Greetings>
+					</div>
+				</div>
+				<div>
+          <h3>Random</h3>
+          <Random min={1} max={6} />
+          <Random min={1} max={100} />
+        </div>
+			</div>
+		);
+	};
+
 }
-
-export default App;
